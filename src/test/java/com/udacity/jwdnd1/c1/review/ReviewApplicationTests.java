207f9704557249e760687d0a7b14a655555fadc9
djd2000 <davidd125@yahoo.com>
2021-11-02 20:06:44 +0000

    Initial commit

---
+++ src/test/java/com/udacity/jwdnd1/c1/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd1.c1.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
